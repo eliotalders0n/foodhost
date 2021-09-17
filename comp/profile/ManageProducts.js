01390e5dc1d0a25dfcddb18a9f3cef07138cb1fa
eliotalders0n <eliot.alderson20@gmail.com>
2021-09-17 21:32:52 +0200

    sep17 21:32

--- comp/profile/ManageProducts.js
+++ comp/profile/ManageProducts.js
@@ -29,38 +29,94 @@ function ManageProducts() {
           };
           asd.push(sdf);
         });
+        console.log("asd", asd);
         setDocs(asd);
       });
   }, []);
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      onPress={() => navigation.navigate("viewProduct", { item })}
-      style={{
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginVertical: 10,
-        backgroundColor: COLORS.white,
-      }}
-    >
-      <Text style={{ paddingHorizontal: 20, ...FONTS.h4, color: COLORS.black }}>
-        {item.produce}
-      </Text>
-      <Text style={{ paddingHorizontal: 20, ...FONTS.h6, color: COLORS.black }}>
-        {item.produce_category}
-      </Text>
-      <Text
-        style={{ paddingHorizontal: 20, ...FONTS.h6, color: COLORS.secondary }}
-      >
-        Price: {item.price}
-      </Text>
-    </TouchableOpacity>
-  );
+  const renderItem = ({ item }) => {
+    if (item.createdAt === new Date(Date.now()).toString().slice(0, 15)) {
+      console.log(item.createdAt);
+      return (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("viewProduct", { item })}
+          style={{
+            paddingVertical: 10,
+            borderRadius: 10,
+            marginVertical: 10,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h4, color: COLORS.black }}
+          >
+            {item.produce}
+          </Text>
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h6, color: COLORS.black }}
+          >
+            {item.produce_category}
+          </Text>
+          <Text
+            style={{
+              paddingHorizontal: 20,
+              ...FONTS.h6,
+              color: COLORS.secondary,
+            }}
+          >
+            Price: {item.price}
+          </Text>
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h4, color: COLORS.black }}
+          >
+            listed
+          </Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("viewProduct", { item })}
+          style={{
+            paddingVertical: 10,
+            borderRadius: 10,
+            marginVertical: 10,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h4, color: COLORS.black }}
+          >
+            {item.produce}
+          </Text>
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h6, color: COLORS.black }}
+          >
+            {item.produce_category}
+          </Text>
+          <Text
+            style={{
+              paddingHorizontal: 20,
+              ...FONTS.h6,
+              color: COLORS.secondary,
+            }}
+          >
+            Price: {item.price}
+          </Text>
+          <Text
+            style={{ paddingHorizontal: 20, ...FONTS.h4, color: COLORS.black }}
+          >
+            unlisted
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+  };
 
   return (
     <View style={{ padding: SIZES.padding * 2, height: "100%" }}>
       <Text style={{ ...FONTS.h5, marginVertical: 10 }}>
-        You can view, edit and delete your items from here
+        You can view, edit, re-post and delete your items from here
       </Text>
       {docs && (
         <FlatList
